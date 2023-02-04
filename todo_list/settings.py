188d8f03a25553a4bfde7b763f97bfec07b9f5ae
whosgotch <komovaar@gmail.com>
2023-02-04 23:39:18 +0200

    Finished todolist Django webapp

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "todo_list.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'LwS1lnqhxYtEGnR4o1GF',
+        'HOST': 'containers-us-west-112.railway.app',
+        'PORT': '6018',
     }
 }
 
